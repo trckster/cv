5a7398a1547bd3922069f0ddf3209fccba5f32c0
Daniil <daniil@vivaldi.net>
2023-12-01 20:35:05 +0400

    Add download button

--- main.js
+++ main.js
@@ -10,3 +10,16 @@ window.copyEmail = async () => {
     await navigator.clipboard.writeText(email)
     showCopyNotification()
 }
+
+window.showDownloadOptions = () => {
+    const element = document.getElementById('download-options')
+    element.style.display = element.style.display === 'block' ? 'none' : 'block'
+}
+
+document.addEventListener('mouseup', (e) => {
+    const downloadButton = document.getElementsByClassName('download')[0]
+
+    if (!downloadButton.contains(e.target)) {
+        document.getElementById('download-options').style.display = 'none'
+    }
+})
